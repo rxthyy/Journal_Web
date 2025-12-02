dd439dc2d6941a25904ad61b2aceca87d50a74ed
Ruth Copino <ruth.copino07@gmail.com>
2025-12-02 19:44:25 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  preview: {
+    host: true, // allow all hosts
+    port: parseInt(process.env.PORT) || 4173,
+    strictPort: true,
+  },
+})
